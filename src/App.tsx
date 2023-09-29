f0a379c38ea1966023f8d7693c10987e0f26ba6b
25Pi25 <varunmash@outlook.com>
2023-09-28 23:57:10 -0400

    beat markers

--- src/App.tsx
+++ src/App.tsx
@@ -3,31 +3,31 @@ import './App.css'
 import { Song, TabType } from './types'
 import { MetadataInput } from './components/MetadataInput';
 import MapEditor from './components/MapEditor';
+import devSong from '../public/song.json';
 
 export default function App() {
-  const [song, setSong] = useState<Song>();
+  const [song, setSong] = useState<Song>(devSong);
   const [tab, setTab] = useState<TabType>("Mapping");
   const [ogg, setOgg] = useState<File>();
-  if (!song) {
-    return ogg ? <MetadataInput ogg={ogg} setSong={setSong} /> :
-      <div>
-        <h1>Upload an OGG File</h1>
-        <h2>For starting a project</h2>
-        <input
-          type="file"
-          accept=".ogg"
-          onChange={({ target: { files } }) => setOgg(files?.[0])}
-        />
-        <h1>Upload a Project JSON</h1>
-        <h2>For continuing an existing project</h2>
-        <input
-          type="file"
-          accept=".json"
-          onChange={async ({ target: { files } }) => files && setSong(JSON.parse(await files?.[0].text()) as Song)}
-        />
-      </div>
-  }
-
+  // if (!song) {
+  //   return ogg ? <MetadataInput ogg={ogg} setSong={setSong} /> :
+  //     <div>
+  //       <h1>Upload an OGG File</h1>
+  //       <h2>For starting a project</h2>
+  //       <input
+  //         type="file"
+  //         accept=".ogg"
+  //         onChange={({ target: { files } }) => setOgg(files?.[0])}
+  //       />
+  //       <h1>Upload a Project JSON</h1>
+  //       <h2>For continuing an existing project</h2>
+  //       <input
+  //         type="file"
+  //         accept=".json"
+  //         onChange={async ({ target: { files } }) => files && setSong(JSON.parse(await files?.[0].text()) as Song)}
+  //       />
+  //     </div>
+  // }
   return <>
     <div>
       {["Mapping", "Metadata"].map((tabOption) =>
--- src/components/MapEditor.tsx
+++ src/components/MapEditor.tsx
@@ -1,64 +1,89 @@
-import { Component } from 'react';
+import { Component, Fragment } from 'react';
 import "./MapEditor.css"
-import { Stage, Layer, Rect, Circle } from 'react-konva';
+import { Stage, Layer, Rect, Circle, Text } from 'react-konva';
 import { Song } from '../types';
 import { KonvaEventObject } from 'konva/lib/Node';
 
 interface Props {
-    song: Song
+  song: Song
 }
 interface State {
-    scroll: number
+  scroll: number
 }
 export default class MapEditor extends Component<Props, State> {
-    public song: Song;
-    public LINES_BORDER = 200;
-    public WIDTH = 800;
-    public HEIGHT = 600;
-    state: State = {
-        scroll: 0
-    }
+  public song: Song;
+  public LINES_BORDER = 200;
+  public WIDTH = 800;
+  public HEIGHT = 600;
+  state: State = {
+    scroll: 500
+  }
 
-    constructor(props: Props) {
-        super(props);
-        const { song } = props;
-        this.song = song;
-    }
-    render() {
-        return <div className='map'>
-            <Stage width={this.WIDTH} height={this.HEIGHT} onWheel={e => this.handleWheel(e)}>
-                <Layer>
-                    {...Array.from({ length: 5 }, (_, i) => {
-                        return <Rect
-                            key={i}
-                            x={this.LINES_BORDER + (this.WIDTH - this.LINES_BORDER * 2) / 4 * i}
-                            width={3}
-                            height={this.HEIGHT}
-                            offset={{ x: 1.5, y: 0 }}
-                            fill="white"
-                        />
-                    })}
-                </Layer>
-                <Layer>
-                    <Circle
-                        x={200}
-                        y={this.state.scroll}
-                        width={50}
-                        height={50}
-                        fill="red"
-                    />
-                    <Circle
-                        x={400}
-                        y={this.state.scroll + 200}
-                        width={50}
-                        height={50}
-                        fill="red"
-                    />
-                </Layer>
-            </Stage>
-        </div>
-    }
-    handleWheel({ evt: { deltaY } }: KonvaEventObject<WheelEvent>) {
-        this.setState(({ scroll }) => ({ scroll: scroll - deltaY }))
-    }
+  constructor(props: Props) {
+    super(props);
+    const { song } = props;
+    this.song = song;
+  }
+  render() {
+    return <div className='map'>
+      <Stage width={this.WIDTH} height={this.HEIGHT} onWheel={e => this.handleWheel(e)}>
+        <Layer>
+          {...Array.from({ length: 5 }, (_, i) => {
+            return <Rect
+              key={i}
+              x={this.LINES_BORDER + (this.WIDTH - this.LINES_BORDER * 2) / 4 * i}
+              width={3}
+              height={this.HEIGHT}
+              offset={{ x: 1.5, y: 0 }}
+              fill="white"
+            />
+          })}
+        </Layer>
+        <Layer>
+          {...Array.from({ length: 100 }, (_, i) =>
+            <Fragment key={i}>
+              <Rect
+                x={this.WIDTH / 2}
+                y={-400 * i + this.state.scroll}
+                width={this.WIDTH * 5 / 8}
+                height={2}
+                offset={{ x: this.WIDTH * 5 / 16, y: 0 }}
+                fill="rgba(255,255,255,0.5)"
+              />
+              <Text
+                x={this.WIDTH / 2}
+                y={-400 * i + this.state.scroll}
+                offset={{ x: -this.WIDTH * 3 / 9, y: 15 }}
+                fill="rgba(255,255,255,0.5)"
+                text={i.toString()}
+                fontFamily='Inter'
+                fontSize={30}
+                align='center'
+                verticalAlign='middle'
+              />
+            </Fragment>
+          )}
+        </Layer>
+        <Layer>
+          <Circle
+            x={200}
+            y={this.state.scroll}
+            width={50}
+            height={50}
+            fill="red"
+          />
+          <Circle
+            x={400}
+            y={this.state.scroll - 200}
+            width={50}
+            height={50}
+            fill="red"
+          />
+        </Layer>
+      </Stage>
+    </div>
+  }
+  handleWheel({ evt: { deltaY } }: KonvaEventObject<WheelEvent>) {
+    this.setState(({ scroll }) => ({ scroll: Math.max(scroll - deltaY, 0) }))
+  }
 }
